92853be22b365463542ad124f53743ec652ef4c2
Daniel <daniel.russi@hotmail.com>
2022-10-04 12:12:19 -0500

    m

--- pages/api/products/generateQR.js
+++ pages/api/products/generateQR.js
@@ -1,85 +1,85 @@
-import nc from "next-connect";
-import QRCode from "qrcode";
-import { isAuth } from "../../../utils/auth";
-import config from "../../../utils/config";
-import axios from "axios";
+// import nc from "next-connect";
+// import QRCode from "qrcode";
+// import { isAuth } from "../../../utils/auth";
+// import config from "../../../utils/config";
+// import axios from "axios";
 
-const handler = nc();
-handler.use(isAuth);
+// const handler = nc();
+// handler.use(isAuth);
 
-handler.post(async (req, res) => {
-  const { evento, ...resBody } = req.body;
-  const projectId = config.projectId;
-  const dataset = config.dataset;
-  const tokenWithWriteAccess = process.env.SANITY_AUTH_TOKEN;
-  try {
-    const { data } = await axios.post(
-      `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}?returnIds=true`,
-      {
-        mutations: [
-          {
-            create: {
-              _type: "orderItem",
-              ticketsAvailable: resBody.quantity,
-              evento: {
-                _type: "reference",
-                _ref: evento.id,
-              },
-            },
-          },
-        ],
-      },
-      {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: `Bearer ${tokenWithWriteAccess}`,
-        },
-      }
-    );
+// handler.post(async (req, res) => {
+//   const { evento, ...resBody } = req.body;
+//   const projectId = config.projectId;
+//   const dataset = config.dataset;
+//   const tokenWithWriteAccess = process.env.SANITY_AUTH_TOKEN;
+//   try {
+//     const { data } = await axios.post(
+//       `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}?returnIds=true`,
+//       {
+//         mutations: [
+//           {
+//             create: {
+//               _type: "orderItem",
+//               ticketsAvailable: resBody.quantity,
+//               evento: {
+//                 _type: "reference",
+//                 _ref: evento.id,
+//               },
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         headers: {
+//           "Content-type": "application/json",
+//           Authorization: `Bearer ${tokenWithWriteAccess}`,
+//         },
+//       }
+//     );
 
-    console.log(data);
-    const resImage = await QRCode.toDataURL(
-      `localhost:3000/pruebaQR/${data.results[0].id}`
-    );
-    console.log(resImage);
+//     console.log(data);
+//     const resImage = await QRCode.toDataURL(
+//       `localhost:3000/pruebaQR/${data.results[0].id}`
+//     );
+//     console.log(resImage);
 
-    const { data: dataOrder } = await axios.post(
-      `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}?returnIds=true`,
-      {
-        mutations: [
-          {
-            create: {
-              _type: "order",
-              createdAt: new Date().toISOString(),
-              ...resBody,
-              imageQR: resImage,
-              user: {
-                _type: "reference",
-                _ref: req.user._id,
-              },
-              orderItem: {
-                _type: "reference",
-                _ref: data.results[0].id,
-              },
-            },
-          },
-        ],
-      },
-      {
-        headers: {
-          "Content-type": "application/json",
-          Authorization: `Bearer ${tokenWithWriteAccess}`,
-        },
-      }
-    );
+//     const { data: dataOrder } = await axios.post(
+//       `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}?returnIds=true`,
+//       {
+//         mutations: [
+//           {
+//             create: {
+//               _type: "order",
+//               createdAt: new Date().toISOString(),
+//               ...resBody,
+//               imageQR: resImage,
+//               user: {
+//                 _type: "reference",
+//                 _ref: req.user._id,
+//               },
+//               orderItem: {
+//                 _type: "reference",
+//                 _ref: data.results[0].id,
+//               },
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         headers: {
+//           "Content-type": "application/json",
+//           Authorization: `Bearer ${tokenWithWriteAccess}`,
+//         },
+//       }
+//     );
 
-    console.log(dataOrder);
-    res.status(200).json(dataOrder);
-  } catch (error) {
-    console.log(error);
-  }
+//     console.log(dataOrder);
+//     res.status(200).json(dataOrder);
+//   } catch (error) {
+//     console.log(error);
+//   }
 
-  // res.send(colecciones);
-});
+//   // res.send(colecciones);
+// });
 
-export default handler;
+// export default handler;
--- pages/prueba.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function prueba() {
-  return <div>hello</div>;
-}
