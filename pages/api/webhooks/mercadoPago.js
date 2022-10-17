df3f5fd9a67f4bd66c307a2522e982adbd73c0c5
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2022-10-16 21:56:58 -0500

    commit kk

--- pages/api/webhooks/mercadoPago.js
+++ pages/api/webhooks/mercadoPago.js
@@ -5,16 +5,16 @@ import axios from "axios";
 const handler = nc();
 handler.post(async (req, res) => {
   const { user_id,type,data:{id} } = req.body;
-  const compra = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`,{
-    headers: {
-      "Content-type": "application/json",
-      Authorization: `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}`,
-    },
-  });
-  console.log(compra);
-
-
+  
+  
   try {
+    const compra = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`,{
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${process.env.MERCADO_PAGO_ACCESS_TOKEN}`,
+      },
+    });
+    console.log(compra);
     const orderItem = await client.fetch(`*[_type == "orderItem" && userMercadoPago == $idOrder]`, {
       idOrder: user_id,
     });
