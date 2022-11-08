f7d5f14a3f6a50679afa11d0193e1966806cc607
Daniel <daniel.russi@hotmail.com>
2022-11-08 12:40:22 -0500

    m

--- pages/compradores.js
+++ pages/compradores.js
@@ -251,7 +251,6 @@ const Compradores = () => {
                   required: true,
                   pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
                 }}
-                
                 render={({ field }) => (
                   <TextField
                     margin="normal"
