f89c59eb94c303ae699af204c6bffc928bb89768
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2022-11-08 11:37:09 -0500

    raro

--- pages/compradores.js
+++ pages/compradores.js
@@ -251,6 +251,7 @@ const Compradores = () => {
                   required: true,
                   pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
                 }}
+                
                 render={({ field }) => (
                   <TextField
                     margin="normal"
