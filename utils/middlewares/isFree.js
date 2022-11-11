6c531ca4248cea9e0512e1e4c2267bcbfa622d10
guille1999utp <90706729+guille1999utp@users.noreply.github.com>
2022-11-11 01:50:41 -0500

    adelantos
    diseno ticket 70% responsive con datis

--- utils/middlewares/isFree.js
+++ utils/middlewares/isFree.js
@@ -3,6 +3,7 @@ import client from "../client";
 import config from "../config";
 import transporter from "../nodemailer";
 import QRCode from "qrcode";
+import { urlFor } from "../image";
 const isFree = async (req, res, next) => {
   const { evento, users } = req.body;
   const projectId = config.projectId;
@@ -53,6 +54,28 @@ const isFree = async (req, res, next) => {
           }
         );
 
+        await axios.post(
+          `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}`,
+          {
+            mutations: [
+              {
+                patch: {
+                  id: evento,
+                  inc: {
+                    ticketVendidos: 1,
+                  },
+                },
+              },
+            ],
+          },
+          {
+            headers: {
+              "Content-type": "application/json",
+              Authorization: `Bearer ${tokenWithWriteAccess}`,
+            },
+          }
+        );
+
         const resImage = await QRCode.toDataURL(
           `localhost:3000/pruebaQR/${data.results[0].id}`
         );
@@ -60,33 +83,34 @@ const isFree = async (req, res, next) => {
         await transporter.sendMail({
           from: `"inputlatam@gmail.com" <${process.env.CORREO_SECRET}>`, // sender address
           to: users[0].correo, // list of receivers
-          subject: `inputlatam.com -> Entrada Cube Ventures`, // Subject line
+          subject: `inputlatam.com -> Entrada ${event[0].nombre}`, // Subject line
           text: "", // plain text body
           html: `
           <div style="width: 100%;background-color: rgb(52, 200, 113);padding: 50px;box-sizing: border-box;">
           <h1 style="margin: 0px 0 10px 0;font-size: 55px;font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">INPUT</h1>
           <div
               style="width: 100%;border: 9px solid black;background-color: white;padding: 50px 40px 20px 40px;box-sizing: border-box;">
-              <h1 style="margin: 0;font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;font-size: 35px;">CUBE VENTURES PARTY</h1>
+              <h1 style="margin: 0;font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;font-size: 35px;">${event[0].nombre}</h1>
               <div style="display: flex;flex-direction: row;justify-content: space-between;">
-                  <div style="display: flex;flex-direction: column;width: 30%;">
+                  <div style="width: 50%;min-width: 50vh;">
                       <p style="font-size: 29px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Jueves 17 nov,2022</p>
-                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Hora: 08:00 p.m.</p>
-                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Movistar Arena,Bogota</p>
+                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Hora: ${event[0].hora}</p>
+                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">${event[0].lugar},${event[0].ciudad}</p>
                       <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Acceso general: $0.00</p>
                       <img style="width: 70%;"
                       src="cid:unique@nodemailer.com" />
                   </div>
-                  <div style="display: flex;flex-direction: column;width: 30%;">
-                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Titular: Jorge de la Hoz</p>
-                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">ID: 1020982734</p>
-                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Ticket No. 073</p>
+                  <div style="width: 50%;min-width: 50vh;">
+                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Titular: ${event[0].artista}</p>
+                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">ID: ${data.results[0].id}</p>
+                      <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Ticket No. ${event[0].totalTickets - (event[0].totalTickets - (i+1+event[0].ticketVendidos))}</p>
                       <p style="font-size: 25px;margin: 10px 0;font-family: Arial, Helvetica, sans-serif;">Orden No. 882138</p>
-                  </div>
-                  <div style="width: 40%;">
+                      <div>
                       <img style="width: 100%;border: 4px solid black;padding: 40px;box-sizing: border-box;"
-                          src="https://th.bing.com/th/id/R.28368da37bcf6d8d8b009cc46b6ce194?rik=v2rNqkZSRehWVA&pid=ImgRaw&r=0" />
+                          src="${urlFor(event[0].image[0])}" />
                   </div>
+                  </div>
+                  
               </div>
           </div>
           <h3 style="color: white;font-size: 30px;font-family: Arial, Helvetica, sans-serif;font-weight: 300;">AVISO:</h3>
